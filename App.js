e8b59a1e21c5f56445908c8bece553ed1f573365
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-05-28 16:48:37 -0700

    Added basic components and inline styling

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    // A view is the equivalent of a div
+
+    <View style={{ padding: 50 }}>
+      <View style={{ flexDirection: "row", justifyContent: 'space-between', alignItems: "center" }}>
+        <TextInput placeholder="Course Goal" style={{ width: '80%', borderBottomColor: 'black', borderWidth: 1 }} />
+        <Button title="ADD" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+
 });
